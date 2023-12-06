bd91d961b93bca996693dbe73b3f79dc6018dba2
MoritzSchwerer <moritz.schwerer@gmx.de>
2023-12-06 16:47:11 +0100

    made code more readable

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # gemm_opt
 This is a toy project where I tried to optimize a Matrix-Matrix multiplication in c
+
+
+### Time 1024x1024
+- prim takes rougly 14 seconds
+- v1 with swapped loop order takes rougly 3 seconds
--- v1.c
+++ v1.c
@@ -2,62 +2,41 @@
 #include <stdlib.h>
 #include <sys/time.h>
 
-void prim_mul(float** result, float** x, float** y, int num_elems) {
-    for (int i = 0; i < num_elems; i++) {
-        for (int j = 0; j < num_elems; j++) {
-            for (int k = 0; k < num_elems; k++) {
-                result[i][j] += x[i][k] * y[k][j];
+#define N 1024
+
+float A[N*N] __attribute__ ((aligned (64)));
+float B[N*N] __attribute__ ((aligned (64)));
+float C[N*N] __attribute__ ((aligned (64)));
+float TEMP[N*N] __attribute__ ((aligned (64)));
+
+void swapped_loop_mul() {
+    for (int i = 0; i < N; i++) {
+        for (int k = 0; k < N; k++) {
+            for (int j = 0; j < N; j++) {
+                C[(i*N) + j] += A[(i*N) + k] * B[(k*N) + j];
             }
         }
     }
 }
 
-void swap_loops_mul(float** result, float** x, float** y, int num_elems) {
-    for (int i = 0; i < num_elems; i++) {
-        for (int k = 0; k < num_elems; k++) {
-            for (int j = 0; j < num_elems; j++) {
-                result[i][j] += x[i][k] * y[k][j];
-            }
-        }
+void init_matrix() {
+    for (int i = 0; i < N*N; i++) {
+        A[i] = (float)(rand() % 1000000) / 1000000.0f;
+        B[i] = (float)(rand() % 1000000) / 1000000.0f;
+        C[i] = 0.0f;
+        TEMP[i] = 0.0f;
     }
-}
-
-
-float** init_matrix(int n, int zero_init) {
-    float** matrix = (float**)malloc(n * sizeof(float*));
-
-    for (int i = 0; i < n; i++) {
-        matrix[i] = (float*)malloc(n * sizeof(float));
-    }
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (zero_init) {
-                matrix[i][j] = 0.0f;
-            } else {
-                matrix[i][j] = (float)(rand() % 1000000) / 1000000.0f;
-
-            }
-        }
-    }
-    return matrix;
 }
 
 int main() {
-    int n = 1024;
-    float** x = init_matrix(n, 0);
-    float** y = init_matrix(n, 0);
-    float** res = init_matrix(n, 1);
+    init_matrix();
 
     struct timeval start_time, end_time;
     gettimeofday(&start_time, NULL);
-
-    swap_loops_mul(res, x, y, n);
-
+    swapped_loop_mul();
     gettimeofday(&end_time, NULL);
     double elapsed_time = (end_time.tv_sec - start_time.tv_sec) +
                           (end_time.tv_usec - start_time.tv_usec) / 1e6;
-
     printf("Elapsed time: %f seconds\n", elapsed_time);
 
     return 0;
